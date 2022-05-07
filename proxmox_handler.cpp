e4b57f498418adb4d76fe86fd7723997ac1cfb12
Kurt <km@flyheli.de>
2022-05-07 02:06:27 +0200

    proxman

--- proxmox_handler.cpp
+++ proxmox_handler.cpp
@@ -11,7 +11,7 @@ extern int wsconnected;
 #define NODE_IP     "192.168.10.90"
 #define NODE_NAME   "proxmoxnuc"
 #define USER_NAME   "root@pam"
-#define USER_PASS   "ivibauul"
+#define USER_PASS   "mypassword"
 #define NODE_PORT   8006
 #define PROXY_PORT  3128
 // =========================================
